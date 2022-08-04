229f24e06c4ec433484b78811e0a41b8ecb3e7db
Gregory Petrov <grishap96123@gmail.com>
2022-08-04 15:15:18 +0300

    added new params to result

--- src/controllers/films.js
+++ src/controllers/films.js
@@ -3,22 +3,23 @@ const axios = require('axios').default;
 const { createQueryStr } = require('../utils/utils');
 const ErrorResponse = require('../utils/ErrorResponse');
 
-const OMDB_API_URL = `${process.env.BASE_API_URL}/?apikey=${process.env.OMDB_API_KEY}`;
-
 //@desc Get films including query params
 //@route GET /films
 //@access Public
 exports.getListFilms = asyncHandler(async (req, res, next) => {
   const result = await axios.get(
-    `${OMDB_API_URL}&${createQueryStr(req.query)}`
+    `${process.env.OMDB_API_URL}&${createQueryStr(req.query)}`
   );
   if (result.data.Response == 'False') {
     return next(new ErrorResponse(result.data.Error, 400));
   }
+  console.log(result);
 
   res.status(200).json({
     success: true,
-    total: result.data.totalResults,
+    total: Number(result.data.totalResults),
+    page: req.query.p,
+    count: result.data.Search.length,
     data: result.data.Search,
   });
 });
@@ -27,7 +28,9 @@ exports.getListFilms = asyncHandler(async (req, res, next) => {
 //@route GET /films/:id
 //@access Public
 exports.getFilm = asyncHandler(async (req, res, next) => {
-  const result = await axios.get(`${OMDB_API_URL}&i=${req.params.id}`);
+  const result = await axios.get(
+    `${process.env.OMDB_API_URL}&i=${req.params.id}`
+  );
   if (result.data.Response == 'False') {
     return next(new ErrorResponse(result.data.Error, 400));
   }

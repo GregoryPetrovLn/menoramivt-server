b3ffbb2601e2bf8ca163f95707a8464696875b5b
Gregory Petrov <grishap96123@gmail.com>
2022-08-04 15:07:06 +0300

    removed unnecessary

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -3,28 +3,7 @@ const errorHandler = (err, req, res, next) => {
   let error = { ...err };
   error.message = err.message;
   error.name = err.constructor.name;
-
-  // Log to console for dev
-  console.log('error===>', error);
-  // // Mongoose bad ObjectId
-  // if (error.name === 'CastError') {
-  //   const message = `Resource not found with id of ${error.value}`;
-  //   error = new ErrorResponse(message, 404);
-  // }
-
-  // //Mongoose duplicate key
-  // if (error.code === 11000) {
-  //   const message = `Duplicate field value entered`;
-  //   error = new ErrorResponse(message, 400);
-  // }
-
-  // //Mongoose validation
-  // if (error.name === 'ValidationError') {
-  //   const message = Object.values(error.errors).map(
-  //     (val) => val.properties.message
-  //   );
-  //   error = new ErrorResponse(message, 400);
-  // }
+  console.log(error);
 
   res.status(error.statusCode || 500).json({
     success: false,

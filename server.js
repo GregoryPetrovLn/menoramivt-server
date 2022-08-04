6707d618db02d13708273569d63e24c0938d629f
Gregory Petrov <grishap96123@gmail.com>
2022-08-04 14:35:58 +0300

    added error to middleware

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 const express = require('express');
-const path = require('path');
+const errorHandler = require('./src/middleware/error');
 
 //Load env files
 const dotenv = require('dotenv');
 dotenv.config({ path: './config/config.env' });
 
 //Import routes
+const films = require('./src/routes/films');
 
 const app = express();
 
 //Body parser
 app.use(express.json());
 
+//Mount routes
+app.use(`/api/ombdb`, films);
+
+//Middleware error hanlder
+app.use(errorHandler);
+
 //Init server
 const PORT = process.env.PORT || 3300;
 const server = app.listen(PORT, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 );
 
 //Handler unhandled promise rejections
